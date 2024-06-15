5ccd5fa5b0949475640d7fdffded062993a46431
Ruiii08 <145280655+Ruiii08@users.noreply.github.com>
2024-06-15 20:03:44 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-function menu(){
-    const item = document.querySelector('.ham_links');
-    const icon = document.querySelector('.ham_icon');
-    item.classList.toggle("open")
-    icon.classList.toggle("open")
-
-}
